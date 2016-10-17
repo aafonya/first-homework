6a8d2f7955220c1e1797b5a00519cdc9ef97d057
aafonya <tolnai.judit12@gmail.com>
2016-10-17 18:59:10 +0200

    Add files via upload

--- 100Doors.py
+++ 100Doors.py
@@ -1,8 +1,13 @@
+#Create a list#
 doors = []
 
+#Fullfill the list#
+#False - Door is closed#
 for i in range(0,101):
      doors.append(False)
 
+# j - number of the cycle - e.g. in the 2nd cycle every second door will change#
+# i - index of the door#
 for j in range(1,101):
      for i in range(1,101):
          if i%j == 0:
@@ -14,6 +19,7 @@ for j in range(1,101):
 
 print(doors)
 
+#searching every True element (opened door) in the list and print their indexes#
 for i in range(1,101):
     if doors[i] == True:
         print(i)
